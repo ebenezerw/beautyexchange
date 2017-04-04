e87d506fe85d1bee5e56d1f931b635985899747e
Ebenezer Williams <ebenezerw@me.com>
2017-04-04 14:26:54 -0400

    can view register and log in forms.

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -27,12 +27,87 @@ angular.module("beautyExchange", ['ui.router'])
                 }]
             }
         })
+        .state('login', {
+            url: '/login',
+            templateUrl: '/login.html',
+            controller: 'AuthCtrl',
+            onEnter: ['$state', 'auth', function($state, auth){
+                if(auth.isLoggedIn()){
+                  $state.go('home');
+                }
+            }]
+        })
+        .state("register", {
+            url: "/register",
+            templateUrl: "/register.html",
+            controller: "AuthCtrl",
+            onEnter: ["$state", "auth", function ($state, auth) {
+                if (auth.isLoggedIn()) {
+                    $state.go("home")
+                }
+            }]
+        })
 
         $urlRouterProvider.otherwise("home")
     }
 ])
 
-.factory("products", ["$http", function ($http) {
+
+.factory("auth", ["$http", "$window", function ($http, $window) {
+    var auth = {}
+
+    auth.saveToken = function (token) {
+        $window.localStorage["beautyexchange-token"] = token
+    }
+
+    auth.getToken = function () {
+        return $window.localStorage["beautyexchange-token"]
+    }
+
+    auth.isLoggedIn = function () {
+        var token = auth.getToken()
+
+        if (token) {
+            var payload = JSON.parse($window.atob(token.split(".")[1]))
+            return payload.exp > Date.now() / 1000
+        } else {
+            return false
+
+        }
+    }
+
+    auth.currentUser = function () {
+        if (auth.isLoggedIn()) {
+            var token = auth.getToken()
+            var payload = JSON.parse($window.atob(token.split(".")[1]))
+
+            return payload.userName
+        }
+    }
+
+    auth.register = function (user) {
+        return $http.post("/register", user).success(function (data) {
+            auth.saveToken(data.token)
+        })
+    }
+
+    auth.login = function (user) {
+        return $http.post("/login", user).success(function (data) {
+            auth.saveToken(data.token)
+        })
+    }
+
+    auth.logOut = function () {
+        $window.localStorage.removeItem("beautyexchange-token")
+    }
+
+    return auth
+}])
+
+
+
+
+.factory("products", ["$http", function ($http, auth) {
     var o = {
         products: []
     }
@@ -50,21 +125,62 @@ angular.module("beautyExchange", ['ui.router'])
     }
 
     o.create = function (product) {
-        return $http.post("/products", product).success(function (data) {
+        return $http.post("/products", product, {
+            headers: {Authorization: 'Bearer '+auth.getToken()}
+        }).success(function (data) {
             o.products.push(data)
         })
     }
     o.upvote = function (product) {
-        return $http.put("/products/" + product._id + "/upvote").success(function (data) {
+        return $http.put("/products/" + product._id + "/upvote", null, {
+            headers: {Authorization: 'Bearer '+auth.getToken()}
+        }).success(function (data) {
             product.upvotes ++
         })
     }
 
     o.addComment = function (id, comment) {
-        return $http.post("/products/" + id + "/comments", comment)
+        return $http.post("/products/" + id + "/comments", comment, {
+            headers: {Authorization: 'Bearer '+auth.getToken()}
+        })
         }
 
     return o
+}])
+
+.controller("NavCtrl", [
+    "$scope",
+    "auth",
+
+    function ($scope, auth) {
+        $scope.isLoggedIn = auth.isLoggedIn
+        $scope.currentUser = auth.currentUser
+        $scope.logOut = auth.logOut
+    }
+])
+
+.controller('AuthCtrl', [
+    '$scope',
+    '$state',
+    'auth',
+    function($scope, $state, auth){
+        $scope.user = {};
+
+        $scope.register = function(){
+            auth.register($scope.user).error(function(error){
+                $scope.error = error;
+            }).then(function(){
+                $state.go('home');
+            });
+            };
+
+            $scope.logIn = function(){
+                auth.logIn($scope.user).error(function(error){
+                    $scope.error = error;
+                }).then(function(){
+                    $state.go('home');
+                });
+      };
 }])
 
 .controller("MainController", [
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require("express-jwt")
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'})
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -7,8 +12,47 @@ router.get('/', function(req, res) {
 });
 
 var mongoose = require("mongoose")
+var passport = require("passport")
+var jwt = require("express-jwt")
 var Product = mongoose.model("Product")
 var Comment = mongoose.model("Comment")
+var User = mongoose.model("User")
+
+// route to register new user
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password)
+
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
+});
+
+// login route for new users
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
 
 
 // GET request for all products
@@ -22,8 +66,9 @@ router.get("/products", function (req, res, next) {
 
 
 // POST request for new products
-router.post("/products", function (req, res, next) {
+router.post("/products", auth, function (req, res, next) {
     var product = new Product(req.body)
+    product.userName = req.payload.userName
 
     product.save(function (err, product) {
         if (err) {
@@ -69,7 +114,7 @@ router.get("/products/:product", function (req, res) {
 })
 
 // PUT request to update individual products with vote count
-router.put("/products/:product/upvote", function (req, res, next) {
+router.put("/products/:product/upvote", auth, function (req, res, next) {
     req.product.upvote(function (err, product) {
         if (err) {
             return next(err)
@@ -80,9 +125,10 @@ router.put("/products/:product/upvote", function (req, res, next) {
 })
 
 // POST route for adding comments to a product
-router.post("/products/:product/comments", function  (req, res, next) {
+router.post("/products/:product/comments", auth, function  (req, res, next) {
     var comment = new Comment(req.body)
     comment.post = req.product
+    comment.userName = req.payload.userName
 
     comment.save(function (err, comment) {
         if (err) {
